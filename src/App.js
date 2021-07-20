bd1e366f96c302537c7854c50e57b1add13ff889
Luciano Ferreira <l.ferreira1975@hotmail.com>
2021-07-20 11:13:17 -0300

    precos termosinter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { json } from 'body-parser';
+import React, { useEffect, useState } from 'react';
+import './style.css'
 function App() {
+  const [Preco, setPreco] = useState([
+    {id:1949, cliente: 'Arautho', fator: 71.07},
+    {id: 311, cliente: 'Beggiauto', fator: 70.48}
+  ]);
+
+  useEffect(() => {
+
+    function loadApi(){
+//      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+      
+//      fetch(url)
+//      .then((r)=> r.json())
+//      .then((json)=> {
+//        console.log(json);
+//        setPreco(json)
+//      })
+
+    }
+
+    loadApi();
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header>
+        <strong>Termosinter Preços</strong>
+    </header>
+
+    {Preco.map((item)=>{
+        return(
+          <article key={item.id} className="post">
+            <strong className="titulo">{item.cliente}</strong>
+            <strong>{item.fator}</strong>
+          </article>
+        )
+
+
+    })}
+
     </div>
   );
 }
